a9727ddba1e3a5fed1ade6f7bb54de3671bee0e0
Soarezzsemj <popcornor6topster@outlook.com>
2025-06-05 17:42:30 -0300

    primeira parte

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Start from './components/Start';
 
 function App() {
   return (
-    <div className="App">
-
-    <Start/>
-      
-   
+    <div className="terminal">
+      <div className="terminal-header">
+        <div className="button close"></div>
+        <div className="button minimize"></div>
+        <div className="button maximize"></div>
+      </div>
+      <div className="terminal-body">
+        <Start/>
+      </div>
     </div>
   );
 }
