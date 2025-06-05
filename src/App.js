e9d0ae97377244c908a4d9814f9aec4da735ba3d
Soarezzsemj <popcornor6topster@outlook.com>
2025-06-05 17:20:56 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Start from './components/Start';
+
+function App() {
+  return (
+    <div className="App">
+
+    <Start/>
+      
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Start.js
@@ -0,0 +1,26 @@
+import Style from './Start.module.css';
+
+function Start(){
+
+return(
+
+    <div className={Style.principal}>
+         <h1>Hello, World!</h1>
+        <h2>const dev = "Carlos Eduardo"; // Software Engineering student & tech enthusiast</h2>
+        <div>
+            
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="" width="30px"/>
+          
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" alt="" width="30px" />
+          
+            <img src="https://www.svgrepo.com/show/506668/instagram.svg"  alt="" width="35px" />
+        </div>
+    
+    </div>
+
+
+)
+
+}
+
+export default Start;
