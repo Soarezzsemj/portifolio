e5213bcf0c75d3091daab7dabac45003a75f3c95
Soarezzsemj <popcornor6topster@outlook.com>
2025-06-11 16:30:49 -0300

    texto animado do começo

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,17 +1,32 @@
 import Style from './Start.module.css';
+import DecryptedText from '../reactbits/TextAnimations/DecryptedText/DecryptedText';
 
 function Start(){
 
 return(
+  <div className={Style.start}>
+
+  <div style={{  }}>
+    <DecryptedText
+    text="Hello, World!"
+    animateOn="view"
+    revealDirection="center"/>
+  </div>
+  <div style={{  }}>
+    <DecryptedText
+    text="const dev = Carlos Eduardo"
+    animateOn="view"
+    revealDirection="center"/>
+  </div>
+  <div style={{  }}>
+    <DecryptedText
+    text="Software Engineering student & tech enthusiast"
+    animateOn="view"
+    revealDirection="center"/>
+  </div>
 
     <div className={Style.principal}>
-         <h1>Hello, World!</h1>
-        <h2>const dev = "Carlos Eduardo";  // Software Engineering student & <br/>   </h2>
-
-        <div className={Style.tech}>
-          <h2> tech enthusiast </h2>
-           
-            </div>
+        
         <div className={Style.icons}>
             <a href="https://github.com/Soarezzsemj" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/ios11/512/FFFFFF/github.png" alt="" width="50px"/>
@@ -25,7 +40,7 @@ return(
             <img src="https://img.icons8.com/win10/512/FFFFFF/instagram-new.png"  alt="" width="50px" />
           </a>
         </div>
-
+</div>
     
     
     </div>
